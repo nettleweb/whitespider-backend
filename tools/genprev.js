7f2583fe3c512828f5261a0887a225d3a0aeb3f9
Ruochen Jia <ruochenjia@outlook.com>
2023-06-04 19:39:04 +0100

    í 

--- tools/genprev.js
+++ tools/genprev.js
@@ -1,47 +1,27 @@
 #!/bin/env node
 // Game preview generator
-
-import puppeteer from "puppeteer";
 import fs from "fs";
-import contents from "../../contents.js";
+import hosts from "../hosts.js";
+import chrome from "../chrome.js";
 
-const chrome = await puppeteer.launch({
-	headless: false,
-	defaultViewport: {
-		width: 800,
-		height: 600,
-		hasTouch: false,
-		isLandscape: true,
-		isMobile: false,
-		deviceScaleFactor: 1
-	},
-	args: [
-		"--disable-gpu",
-		"--window-size=800,600",
-		"--window-position=0,0"
-	],
-	pipe: true,
-	product: "chrome",
-	dumpio: true,
-	timeout: 10000
-});
+for (const line of fs.readFileSync("./static/games/list.txt", "utf-8").split("\n")) {
+	const [ name, type, url ] = line.split(";", 3);
+	const path = "./static/games/previews/" + name + ".jpg";
+	if (fs.existsSync(path)) {
+		continue;
+	}
 
-/**
- * @param {string} out 
- * @param {string | undefined} path 
- * @param {string | undefined} url 
- */
-async function screenshot(out, path, url) {
-	const context = await chrome.createIncognitoBrowserContext({ });
+	const context = await chrome.createIncognitoBrowserContext({});
 	const page = await context.newPage();
 	await page.setCacheEnabled(true);
 	await page.setJavaScriptEnabled(true);
+	await page.setRequestInterception(true);
 	await page.setGeolocation({
 		accuracy: 0,
 		latitude: 0,
 		longitude: 0
 	});
-	await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0", {
+	await page.setUserAgent("Mozilla/5.0 ( ; ; rv:109.0) Gecko/20100101 Firefox/115.0", {
 		architecture: "",
 		bitness: "",
 		brands: [],
@@ -53,37 +33,55 @@ async function screenshot(out, path, url) {
 		platformVersion: "",
 		wow64: false
 	});
+	await page.setViewport({
+		width: 800,
+		height: 600,
+		hasTouch: false,
+		isLandscape: true,
+		isMobile: false,
+		deviceScaleFactor: 1
+	});
 
-	await page.goto(new URL(path == null ? url : path, "https://whitespider.gq").href, {
+	page.on("popup", (page) => page.close({ runBeforeUnload: false }));
+	page.on("request", (request) => {
+		const url = new URL(request.url());
+		switch (url.protocol) {
+			case "http:":
+			case "https:":
+				const host = url.hostname;
+				if (hosts.includes(host)) {
+					request.abort("blockedbyclient");
+					return;
+				}
+				request.continue();
+				break;
+			case "data:":
+			case "blob:":
+				request.continue();
+				break;
+			default:
+				request.abort("accessdenied");
+				break;
+		}
+	});
+
+	await page.goto("https://whitespider.gq/player.html?type=" + type + "&url=" + encodeURIComponent(url), {
 		referer: "",
 		timeout: 10000,
 		waitUntil: "domcontentloaded"
 	});
-	
-	await new Promise(r => setTimeout(r, 15000));
-
+	await new Promise(resolve => setTimeout(resolve, 15000));
 	await page.screenshot({
+		path: path,
 		type: "jpeg",
+		quality: 70,
 		encoding: "binary",
-		fromSurface: true,
 		fullPage: false,
-		omitBackground: true,
-		quality: 50,
-		path: out
+		fromSurface: true,
+		omitBackground: true
 	});
 	await page.close({ runBeforeUnload: false });
 	await context.close();
-}
-
-for (const game of [
-	...contents.dosGames,
-	...contents.flashGames,
-	...contents.html5Games
-]) {
-	const out = `../../preview/${game.name}.jpg`;
-	if (!fs.existsSync(out)) {
-		await screenshot(out, game.path, game.url);
-	}
 }
 
 await chrome.close();
